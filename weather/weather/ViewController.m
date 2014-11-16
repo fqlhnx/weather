ae2ab2d8cf082dd33cd08504ae0b647ee52936da
liuhongnian <860338516@qq.com>
2014-11-16 19:24:46 +0800

    天气demo

--- weather/weather/ViewController.m
+++ weather/weather/ViewController.m
@@ -7,8 +7,11 @@
 //
 
 #import "ViewController.h"
+#import "WethrView.h"
 
 @interface ViewController ()
+
+@property (nonatomic,strong)WethrView *weatherView;
 
 @end
 
@@ -17,11 +20,24 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    [self setupWeatherView];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+#pragma mark prive
+
+- (void)setupWeatherView
+{
+    self.weatherView = [[WethrView alloc] initWithFrame:CGRectMake(134, 28, 160, 228)];
+    self.weatherView.canChangeTempType = YES;
+    self.weatherView.showsTempType = YES;
+    
+    [self.view addSubview:self.weatherView];
 }
 
 @end
