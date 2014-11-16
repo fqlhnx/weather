fad924c7d05c4eb64bd7e33179f7ae814ef930d8
liuhongnian <liuhongnian@liuhongniantekiMacBook-Pro-2.local>
2014-11-16 19:05:20 +0800

    天气

---
+++ weather/weather/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  weather
+//
+//  Created by liuhongnian on 14-11-16.
+//  Copyright (c) 2014年 liuhongnian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ weather/weather/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  weather
+//
+//  Created by liuhongnian on 14-11-16.
+//  Copyright (c) 2014年 liuhongnian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
